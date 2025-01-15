4b67793cb9a19baf7388f9a95e872d4c5f3a750b
Felipe Buiras <f.morisan@gmail.com>
2025-01-15 16:10:55 -0300

    feat: use OZ ECDSA lib instead of naked ecrecover

--- src/SecretHolder.sol
+++ src/SecretHolder.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.20;
 
+import "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+
 /**
  * @notice This contract aims to store secret commitments for two parties, until one of them reveals it.
  * @author Felipe Buiras
@@ -24,18 +26,16 @@ contract SecretHolder {
 
     /// @dev We use a uint256 here as an identifier instead of the salted hash on the off chance that we get a hash collision.
     mapping(uint256 => Secret) public secrets;
-    uint256 secretCount;
+    uint256 public secretCount = 0;
 
+    error InvalidMessageHash();
     error WrongSignatureCount();
-    error InvalidSignature();
+    error BadSignature();
 
     event SecretStored(uint256 indexed id, bytes32 indexed commitment, address partyA, address partyB);
     event SecretRevealed(uint256 indexed id, address indexed revealer, bytes message);
-    event SecretLeaked(uint256 indexed id, address indexed revealer);
 
-    constructor() {
-        secretCount = 0;
-    }
+    constructor() {}
 
     /**
      * @notice Commit a message hash to the contract. Must include signatures from both parties, which may
@@ -46,13 +46,11 @@ contract SecretHolder {
      * @param salt - (uint256) The salt used to calculate the hash.
      * @param signatures - (Signature[2]) ECDSA signatures of the calculated hash from the involved parties.
      */
-    function commitSecret(bytes32 secretHash, uint256 salt, Signature[] calldata signatures) public {
+    function commitSecret(bytes32 secretHash, uint256 salt, bytes[] memory signatures) public {
         require(signatures.length == 2, WrongSignatureCount());
-        address signerA = ecrecover(secretHash, signatures[0].v, signatures[0].r, signatures[0].s);
-        address signerB = ecrecover(secretHash, signatures[1].v, signatures[2].r, signatures[3].s);
 
-        require(signerA != address(0), InvalidSignature());
-        require(signerB != address(0), InvalidSignature());
+        address signerA = ECDSA.recover(secretHash, signatures[0]);
+        address signerB = ECDSA.recover(secretHash, signatures[1]);
 
         secrets[secretCount] = Secret({commitment: secretHash, salt: salt, partyA: signerA, partyB: signerB});
 
@@ -64,29 +62,21 @@ contract SecretHolder {
 
     /**
      * @notice Reveal a previously commited secret.
-     * @dev The stored hash and salt will be deleted from the contract after successful execution.
-     *      If, by any chance, a third party can provide a valid signature for the commited hash,
-     *      we can assume the message was leaked - and there's not much we can do since the message
-     *      will already be revealed for the whole world in calldata anyways.
+     * @dev The stored secret metadata will be deleted from the contract after successful execution.
      * @param id - The identifier of the previously commited secret.
      * @param secretMessage - The commited message, without salting.
      * @param signature - Signature of the revealing party.
      */
-    function revealSecret(uint256 id, bytes calldata secretMessage, Signature calldata signature) public {
+    function revealSecret(uint256 id, bytes calldata secretMessage, bytes memory signature) public {
         Secret storage secret = secrets[id];
 
-        bytes32 hash = keccak256(abi.encode(secretMessage, secret.salt));
+        bytes32 hash = keccak256(abi.encodePacked(secretMessage, secret.salt));
+        require(hash == secret.commitment, InvalidMessageHash());
 
-        address signer = ecrecover(hash, signature.v, signature.r, signature.s);
-        require(signer != address(0), InvalidSignature());
+        address signer = ECDSA.recover(hash, signature);
+        require(signer == secret.partyA || signer == secret.partyB, BadSignature());
 
-        // NOTE: If a third party possesses the message used to generate the hash...
-        //       then the two parties are in *big* trouble. We can't really do anything about that.
-        //       Best we can do, is alert them by raising an event.
         emit SecretRevealed(id, signer, secretMessage);
-        if (signer != secret.partyA && signer != secret.partyB) {
-            emit SecretLeaked(id, signer);
-        }
 
         delete secrets[id];
     }
