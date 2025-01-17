0452148c96bd91dab3194f2a6c8e6910e38088d8
Felipe Buiras <f.morisan@gmail.com>
2025-01-17 01:29:51 -0300

    feat: use EIP-712 signatures instead of naked ECDSA

--- src/SecretHolder.sol
+++ src/SecretHolder.sol
@@ -1,27 +1,21 @@
 pragma solidity 0.8.26;
 
 import "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import "openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
 
 /**
  * @notice This contract aims to store secret commitments for two parties, until one of them reveals it.
  * @author Felipe Buiras
  */
-contract SecretHolder {
+contract SecretHolder is EIP712 {
+    bytes32 private constant SECRET_TYPEHASH = keccak256("Secret(bytes32 hash,uint256 salt)");
+    bytes32 private constant REVEAL_TYPEHASH = keccak256("Reveal(uint256 id,bytes message)");
+
     struct Secret {
         bytes32 commitment;
         address partyA;
         address partyB;
         uint256 salt;
-    }
-
-    /**
-     * @notice Structure holding the passed-in signatures
-     * @dev This helps with destructuring the signature when it comes in via calldata.
-     */
-    struct Signature {
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
     }
 
     /// @dev We use a uint256 here as an identifier instead of the salted hash on the off chance that we get a hash collision.
@@ -35,7 +29,7 @@ contract SecretHolder {
     error WrongSignatureCount();
     error BadSignature();
 
-    constructor() {}
+    constructor() EIP712("SecretHolder", "0.1") {}
 
     /**
      * @notice Commit a message hash to the contract. Must include signatures from both parties, which may
@@ -49,14 +43,21 @@ contract SecretHolder {
     function commitSecret(bytes32 secretHash, uint256 salt, bytes[] memory signatures) external {
         require(signatures.length == 2, WrongSignatureCount());
 
-        address signerA = ECDSA.recover(secretHash, signatures[0]);
-        address signerB = ECDSA.recover(secretHash, signatures[1]);
+        bytes32 hash = buildCommitHash(secretHash, salt);
+
+        address signerA = ECDSA.recover(hash, signatures[0]);
+        address signerB = ECDSA.recover(hash, signatures[1]);
 
         secrets[secretCount] = Secret({commitment: secretHash, salt: salt, partyA: signerA, partyB: signerB});
 
         emit SecretStored(secretCount, secretHash, signerA, signerB);
 
         secretCount++;
+    }
+
+    function buildCommitHash(bytes32 secretHash, uint256 salt) public view returns (bytes32) {
+        bytes32 structHash = keccak256(abi.encode(SECRET_TYPEHASH, secretHash, salt));
+        return _hashTypedDataV4(structHash);
     }
 
     /**
@@ -66,17 +67,25 @@ contract SecretHolder {
      * @param secretMessage - The commited message, without salting.
      * @param signature - Signature of the revealing party.
      */
-    function revealSecret(uint256 id, bytes calldata secretMessage, bytes memory signature) external {
+    function revealSecret(uint256 id, bytes memory secretMessage, bytes memory signature) external {
         Secret storage secret = secrets[id];
+
+        bytes32 revealHash = buildRevealHash(id, secretMessage);
+
+        address signer = ECDSA.recover(revealHash, signature);
+
+        require(signer == secret.partyA || signer == secret.partyB, BadSignature());
 
         bytes32 hash = keccak256(abi.encodePacked(secretMessage, secret.salt));
         require(hash == secret.commitment, InvalidMessageHash());
 
-        address signer = ECDSA.recover(hash, signature);
-        require(signer == secret.partyA || signer == secret.partyB, BadSignature());
-
         emit SecretRevealed(id, signer, secretMessage);
 
         delete secrets[id];
+    }
+
+    function buildRevealHash(uint256 id, bytes memory secretMessage) public view returns (bytes32) {
+        bytes32 structHash = keccak256(abi.encode(REVEAL_TYPEHASH, id, secretMessage));
+        return _hashTypedDataV4(structHash);
     }
 }
--- test/SecretHolder.t.sol
+++ test/SecretHolder.t.sol
@@ -17,48 +17,101 @@ contract CounterTest is Test {
     address carl;
 
     function setUp() public {
-        secretHolder = new SecretHolder();
+        vm.chainId(84532);
+        deployCodeTo("SecretHolder.sol", 0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD);
+        secretHolder = SecretHolder(0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD);
         (alice, alice_pk) = makeAddrAndKey("alice");
         (bob, bob_pk) = makeAddrAndKey("bob");
         (carl, carl_pk) = makeAddrAndKey("carl");
+    }
+
+    function test_commit_externalSign() public {
+        bytes memory signature = hex"64e3b233579cde997a34968868880126bdfa2e14ef438d2f64d3b64f2d2b0ec40199ae0817f87532fca4e15b56af80c0692dfaa5907adbec7f07027528a314611b";
+        uint256 usedSalt = 55413265760448866643891566419717387905414315466329863106176882221108994872920;
+        bytes32 hash = hex"08889276974e17128a7254fa4cad40e2af7cf5e7fe6543e976a35099e7612a82";
+        bytes[] memory sigs = new bytes[](2);
+        sigs[0] = signature;
+        sigs[1] = signature;
+
+        secretHolder.commitSecret(hash, usedSalt, sigs);
+
+        uint256 id = secretHolder.secretCount() - 1;
+
+
     }
 
     function test_constructed() public view {
         assertEq(secretHolder.secretCount(), 0);
     }
 
-    function testFuzz_commit(uint256 salt) public {
+    function test_commit() public {
+        uint256 salt = 0;
         uint256 secretCountBefore = secretHolder.secretCount();
         bytes memory message = "This is a test message";
-        bytes32 hash = keccak256(abi.encodePacked(message, salt));
+        bytes32 secretHash = keccak256(abi.encodePacked(message, salt));
+
+        bytes32 structHash = secretHolder.buildCommitHash(secretHash, salt);
 
         bytes[] memory signatures = new bytes[](2);
-        (uint8 alice_v, bytes32 alice_r, bytes32 alice_s) = vm.sign(alice_pk, hash);
-        (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(bob_pk, hash);
+        (uint8 alice_v, bytes32 alice_r, bytes32 alice_s) = vm.sign(alice_pk, structHash);
+        (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(bob_pk, structHash);
 
         // NOTE: OZ's ECDSA library expects the signature to be formed this way.
         signatures[0] = abi.encodePacked(alice_r, alice_s, alice_v);
         signatures[1] = abi.encodePacked(bob_r, bob_s, bob_v);
 
         vm.expectEmit(false, true, true, true);
-        emit SecretHolder.SecretStored(0, hash, alice, bob);
-        secretHolder.commitSecret(hash, salt, signatures);
+        emit SecretHolder.SecretStored(0, secretHash, alice, bob);
+        secretHolder.commitSecret(secretHash, salt, signatures);
 
         assertEq(secretHolder.secretCount(), secretCountBefore + 1);
+        
+        (bytes32 commitment, address partyA, address partyB, uint256 retrieved_salt) = secretHolder.secrets(secretCountBefore);
+
+        assertEq(commitment, secretHash);
+        assertEq(partyA, alice);
+        assertEq(partyB, bob);
+        assertEq(retrieved_salt, salt);
     }
 
-    function _commitMessage(bytes memory message, uint256 salt) internal returns (uint256 id) {
-        bytes32 hash = keccak256(abi.encodePacked(message, salt));
+    function testFuzz_commit(uint256 pk_1, uint256 pk_2, uint256 salt) public {
+        // NOTE: ECDSA: pk should be in this range to be a valid key
+        vm.assume(pk_1 > 0);
+        vm.assume(pk_1 < 115792089237316195423570985008687907852837564279074904382605163141518161494337);
+        vm.assume(pk_2 > 0);
+        vm.assume(pk_2 < 115792089237316195423570985008687907852837564279074904382605163141518161494337);
+
+        bytes memory message = "This is a test message";
+        bytes32 secretHash = keccak256(abi.encodePacked(message, salt));
+
+        bytes32 structHash = secretHolder.buildCommitHash(secretHash, salt);
 
         bytes[] memory signatures = new bytes[](2);
-        (uint8 alice_v, bytes32 alice_r, bytes32 alice_s) = vm.sign(alice_pk, hash);
-        (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(bob_pk, hash);
+        (uint8 alice_v, bytes32 alice_r, bytes32 alice_s) = vm.sign(pk_1, structHash);
+        (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(pk_2, structHash);
 
         // NOTE: OZ's ECDSA library expects the signature to be formed this way.
         signatures[0] = abi.encodePacked(alice_r, alice_s, alice_v);
         signatures[1] = abi.encodePacked(bob_r, bob_s, bob_v);
 
-        secretHolder.commitSecret(hash, salt, signatures);
+        vm.expectEmit(false, true, true, true);
+        emit SecretHolder.SecretStored(0, secretHash, vm.addr(pk_1), vm.addr(pk_2));
+        secretHolder.commitSecret(secretHash, salt, signatures);
+    }
+
+    function _commitMessage(bytes memory message, uint256 salt) internal returns (uint256 id) {
+        bytes32 secretHash = keccak256(abi.encodePacked(message, salt));
+        bytes32 structHash = secretHolder.buildCommitHash(secretHash, salt);
+
+        bytes[] memory signatures = new bytes[](2);
+        (uint8 alice_v, bytes32 alice_r, bytes32 alice_s) = vm.sign(alice_pk, structHash);
+        (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(bob_pk, structHash);
+
+        // NOTE: OZ's ECDSA library expects the signature to be formed this way.
+        signatures[0] = abi.encodePacked(alice_r, alice_s, alice_v);
+        signatures[1] = abi.encodePacked(bob_r, bob_s, bob_v);
+
+        secretHolder.commitSecret(secretHash, salt, signatures);
 
         id = secretHolder.secretCount() - 1;
     }
@@ -67,13 +120,21 @@ contract CounterTest is Test {
         uint256 salt = 0xFFFF_FFFF;
         uint256 secretId = _commitMessage("This is a test", salt);
 
+        bytes32 revealHash = secretHolder.buildRevealHash(secretId, "This is a test");
+
         // Let's have bob reveal the message
-        bytes32 hash = keccak256(abi.encodePacked("This is a test", salt));
-        (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(bob_pk, hash);
+        (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(bob_pk, revealHash);
 
         vm.expectEmit(true, true, true, true);
         emit SecretHolder.SecretRevealed(secretId, bob, "This is a test");
         secretHolder.revealSecret(secretId, "This is a test", abi.encodePacked(bob_r, bob_s, bob_v));
+
+        // NOTE: Check that the secret metadata has been removed from contract storage.
+        (bytes32 commitment, address partyA, address partyB, uint256 retrieved_salt) = secretHolder.secrets(secretId);
+        assertEq(commitment, bytes32(0));
+        assertEq(partyA, address(0));
+        assertEq(partyB, address(0));
+        assertEq(retrieved_salt, 0);
     }
 
     function testFail_reveal_badMessage() public {
@@ -90,7 +151,7 @@ contract CounterTest is Test {
         uint256 secretId = _commitMessage("This is a test", 0xFFFF_FFFF);
 
         // Let's have bob sign something else entirely
-        bytes32 hash = keccak256(abi.encode("This is another test", 0xFFFF_FFFF));
+        bytes32 hash = secretHolder.buildCommitHash("This is another test", 0xFFFF_FFFF);
         (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(bob_pk, hash);
 
         secretHolder.revealSecret(secretId, "This is a test", abi.encodePacked(bob_r, bob_s, bob_v));
@@ -105,8 +166,9 @@ contract CounterTest is Test {
 
         uint256 secretId = _commitMessage("This is a test", 0xFFFF_FFFF);
 
-        bytes32 hash = keccak256(abi.encode("This is a test", 0xFFFF_FFFF));
-        (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(pk, hash);
+        bytes32 structHash = secretHolder.buildRevealHash(secretId, "This is a test");
+
+        (uint8 bob_v, bytes32 bob_r, bytes32 bob_s) = vm.sign(pk, structHash);
 
         vm.expectRevert();
         secretHolder.revealSecret(secretId, "This is a test", abi.encodePacked(bob_r, bob_s, bob_v));
